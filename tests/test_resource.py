128667c1f0bd8270d803c84aa4292a68c86e806c
Vlad Stefan Munteanu <vladstefanmunteanu@gmail.com>
2020-07-05 20:12:19 +0300

    Coverage back to 100%

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -804,3 +804,200 @@ def test_relationship_many_resource(relationship_many_app: Starlette):
             {'detail': 'Relationship is list-like'}
         ]
     }
+
+
+@pytest.mark.parametrize(['method', 'url'], [
+    ('get', '/test-resource/foo/relationships/rel'),
+    ('patch', '/test-resource/foo/relationships/rel'),
+    ('delete', '/test-resource/foo/relationships/rel'),
+    ('post', '/test-resource/foo/relationships/rel'),
+])
+def test_relationship_resource_default_crud_methods(app: Starlette, method: str, url: str):
+    class TResource(BaseResource):
+        type_ = 'test-resource'
+
+    class TResourceRel(BaseRelationshipResource):
+        parent_resource = TResource
+        relationship_name = 'rel'
+
+    TResource.register_routes(app=app, base_path='/')
+    TResourceRel.register_routes(app=app)
+
+    client = TestClient(app=app)
+    rv = getattr(client, method)(url)
+    assert rv.status_code == 405
+    assert rv.json() == {
+        'errors': [
+            {
+                'detail': 'Method Not Allowed',
+            },
+        ]
+    }
+
+
+def test_relationship_resource_incorrect_field(app: Starlette):
+    class TResourceSchema(JSONAPISchema):
+        class Meta:
+            type_ = 'test-resource'
+        id = fields.Str(dump_only=True)
+
+    class TResource(BaseResource):
+        schema = TResourceSchema
+        type_ = 'test-resource'
+
+    class TResourceRel(BaseRelationshipResource):
+        parent_resource = TResource
+        relationship_name = 'rel'
+
+        async def get(self, parent_id: str, *args, **kwargs) -> Response:
+            """ we inverse status_code to check for the error """
+            try:
+                self._get_relationship_field()
+            except AttributeError as exc:
+                if str(exc) == f'Parent schema does not define relationship {self.relationship_name}.':
+                    return Response(status_code=200)
+            return Response(status_code=500)
+
+    TResource.register_routes(app=app, base_path='/')
+    TResourceRel.register_routes(app=app)
+
+    client = TestClient(app=app)
+    rv = client.get('/test-resource/foo/relationships/rel')
+    assert rv.status_code == 200
+
+
+def test_relationship_resource_expects_content_type(relationship_app: Starlette):
+    test_client = TestClient(relationship_app)
+    rv = test_client.post(
+        '/test-resource/foo/relationships/rel',
+        json={
+            'data': {
+                'type': 'test-related-resource',
+                'id': 'bar'
+            }
+        }
+    )
+    assert rv.status_code == 400
+    assert rv.headers['Content-Type'] == 'application/vnd.api+json'
+    assert rv.json() == {
+        'errors': [
+            {'detail': 'Incorrect or missing Content-Type header, expected `application/vnd.api+json`.'}
+        ]
+    }
+
+
+def test_relationship_resource_expects_valid_json(relationship_app: Starlette):
+    test_client = TestClient(relationship_app)
+    rv = test_client.post(
+        '/test-resource/foo/relationships/rel',
+        headers={'Content-Type': 'application/vnd.api+json'}
+    )
+    assert rv.status_code == 400
+    assert rv.headers['Content-Type'] == 'application/vnd.api+json'
+    assert rv.json() == {
+        'errors': [
+            {'detail': 'Could not read request body.'}
+        ]
+    }
+
+
+def test_handle_error_logs_unhandled_exceptions_in_relationship_resource(app: Starlette, caplog):
+    # this should be useful, we don't want to return 500 exceptions without knowing what the error was.
+    exc = Exception('TestException was raised')
+
+    class TResourceSchema(JSONAPISchema):
+        class Meta:
+            type_ = 'test-resource'
+
+    class TResource(BaseResource):
+        schema = TResourceSchema
+        type_ = 'test-resource'
+
+    class TResourceRel(BaseRelationshipResource):
+        parent_resource = TResource
+        relationship_name = 'rel'
+
+        async def get(self, parent_id: str, *args, **kwargs) -> Response:
+            raise exc
+
+    TResource.register_routes(app=app, base_path='/')
+    TResourceRel.register_routes(app=app)
+
+    test_client = TestClient(app)
+    rv = test_client.get('/test-resource/foo/relationships/rel')
+    assert rv.status_code == 500
+    assert rv.json() == {
+        'errors': [{'detail': 'Internal server error'}]
+    }
+    exception_log_message = (
+        'starlette_jsonapi.resource', logging.ERROR, 'Encountered an error while handling request.'
+    )
+    assert exception_log_message in caplog.record_tuples
+    assert any(log.exc_info[1] == exc and log.name == 'starlette_jsonapi.resource' for log in caplog.records)
+
+
+def test_method_not_allowed_relationship_resource(app: Starlette):
+    class TResourceSchema(JSONAPISchema):
+        class Meta:
+            type_ = 'test-resource'
+
+    class TResource(BaseResource):
+        schema = TResourceSchema
+        type_ = 'test-resource'
+
+    class TResourceRel(BaseRelationshipResource):
+        parent_resource = TResource
+        relationship_name = 'rel'
+        allowed_methods = {'DELETE'}
+
+        async def get(self, parent_id: str, *args, **kwargs) -> Response:
+            return Response(status_code=200)
+
+    TResource.register_routes(app=app, base_path='/')
+    TResourceRel.register_routes(app=app)
+
+    test_client = TestClient(app)
+    rv = test_client.get('/test-resource/foo/relationships/rel')
+    assert rv.status_code == 405
+    assert rv.json() == {
+        'errors': [{'detail': 'Method Not Allowed'}]
+    }
+
+
+def test_relationship_resource_register_routes_missing_parent_type(app: Starlette):
+    class TResourceSchema(JSONAPISchema):
+        class Meta:
+            type_ = 'test-resource'
+
+    class TResource(BaseResource):
+        schema = TResourceSchema
+
+    class TResourceRel(BaseRelationshipResource):
+        parent_resource = TResource
+        relationship_name = 'rel'
+
+    with pytest.raises(Exception) as exc:
+        TResourceRel.register_routes(app=app)
+
+    assert str(exc.value) == (
+        'Cannot register a relationship resource if the `parent_resource` does not specify a `type_`.'
+    )
+
+
+def test_relationship_resource_register_routes_parent_registration_required(app: Starlette):
+    class TResourceSchema(JSONAPISchema):
+        class Meta:
+            type_ = 'test-resource'
+
+    class TResource(BaseResource):
+        schema = TResourceSchema
+        type_ = 'test-resource'
+
+    class TResourceRel(BaseRelationshipResource):
+        parent_resource = TResource
+        relationship_name = 'rel'
+
+    with pytest.raises(Exception) as exc:
+        TResourceRel.register_routes(app=app)
+
+    assert str(exc.value) == 'Parent resource should be registered first.'
