6a8c92c3cd71b4d791069ee74d3b9cdf9bc59c1a
Vlad Stefan Munteanu <vladstefanmunteanu@gmail.com>
2020-12-05 21:10:12 +0200

    Tests for utils.process_sparse_fields

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,10 @@ from starlette.testclient import TestClient
 
 from starlette_jsonapi.exceptions import JSONAPIException
 from starlette_jsonapi.responses import JSONAPIResponse
-from starlette_jsonapi.utils import serialize_error, register_jsonapi_exception_handlers, filter_sparse_fields
+from starlette_jsonapi.utils import (
+    serialize_error, register_jsonapi_exception_handlers, filter_sparse_fields,
+    process_sparse_fields,
+)
 
 
 def test_serialize_error():
@@ -116,3 +119,172 @@ def test_filter_sparse_fields_unknown_field():
     # check that the original data has not been mutated
     assert 'attributes' in jsonapi_repr
     assert 'relationships' in jsonapi_repr
+
+
+def test_process_sparse_fields_single():
+    complete_jsonapi_repr = {
+        'data': {
+            'id': '123',
+            'type': 'users',
+            'attributes': {
+                'name': 'User 1',
+                'country': 'US',
+            },
+            'relationships': {
+                'organization': {
+                    'data': {
+                        'type': 'organizations',
+                        'id': '456',
+                    }
+                }
+            }
+        },
+        'meta': {
+            'key': 'value',
+        }
+    }
+
+    assert process_sparse_fields(
+        complete_jsonapi_repr,
+        many=False, sparse_fields={'users': 'unknown'},
+    ) == {
+        'data': {
+            'id': '123',
+            'type': 'users',
+        },
+        'meta': {
+            'key': 'value',
+        }
+    }
+
+
+def test_process_sparse_fields_many():
+    complete_jsonapi_repr = {
+        'data': [
+            {
+                'id': '1',
+                'type': 'users',
+                'attributes': {
+                    'name': 'User 1',
+                    'country': 'US',
+                },
+                'relationships': {
+                    'organization': {
+                        'data': {
+                            'type': 'organizations',
+                            'id': '1',
+                        }
+                    }
+                }
+            },
+            {
+                'id': '2',
+                'type': 'users',
+                'attributes': {
+                    'name': 'User 2',
+                    'country': 'RO',
+                },
+                'relationships': {
+                    'organization': {
+                        'data': {
+                            'type': 'organizations',
+                            'id': '1',
+                        }
+                    }
+                }
+            },
+        ],
+        'meta': {
+            'key': 'value',
+        }
+    }
+
+    assert process_sparse_fields(
+        complete_jsonapi_repr,
+        many=True, sparse_fields={'users': 'unknown'},
+    ) == {
+        'data': [
+            {
+                'id': '1',
+                'type': 'users',
+            },
+            {
+                'id': '2',
+                'type': 'users',
+            }
+        ],
+        'meta': {
+            'key': 'value',
+        }
+    }
+
+
+def test_process_sparse_fields_included():
+    complete_jsonapi_repr = {
+        'data': {
+            'id': '123',
+            'type': 'users',
+            'attributes': {
+                'name': 'User 1',
+                'country': 'US',
+            },
+            'relationships': {
+                'organization': {
+                    'data': {
+                        'type': 'organizations',
+                        'id': '456',
+                    }
+                }
+            }
+        },
+        'included': [
+            {
+                'id': '456',
+                'type': 'organizations',
+                'attributes': {
+                    'name': 'Organization 1',
+                }
+            }
+        ],
+        'meta': {
+            'key': 'value',
+        }
+    }
+
+    assert process_sparse_fields(
+        complete_jsonapi_repr,
+        many=False, sparse_fields={'users': 'unknown'},
+    ) == {
+        'data': {
+            'id': '123',
+            'type': 'users',
+        },
+        'included': [
+            {
+                'id': '456',
+                'type': 'organizations',
+                'attributes': {
+                    'name': 'Organization 1',
+                }
+            }
+        ],
+        'meta': {
+            'key': 'value',
+        }
+    }
+
+
+def test_process_sparse_fields_without_fields():
+    complete_jsonapi_repr = {
+        'data': {
+            'id': '123',
+            'type': 'users',
+            'attributes': {
+                'name': 'User 1',
+                'country': 'US',
+            }
+        }
+    }
+
+    assert process_sparse_fields(complete_jsonapi_repr, many=False) == complete_jsonapi_repr
+    assert process_sparse_fields(complete_jsonapi_repr, many=False, sparse_fields={}) == complete_jsonapi_repr
